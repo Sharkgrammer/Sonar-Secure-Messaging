2cd7f6c4ec9a5d93178eed399e7c08b01f11861c
Sharkgrammer <danielkeanekelly@gmail.com>
2020-03-13 22:50:31 +0000

    Select random auth server

--- app/src/main/java/com/shark/sonar/activity/MainActivity.java
+++ app/src/main/java/com/shark/sonar/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package com.shark.sonar.activity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,7 +20,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shark.sonar.R;
-import com.shark.sonar.controller.ColourDbControl;
 import com.shark.sonar.controller.ConvoDbControl;
 import com.shark.sonar.controller.DbControl;
 import com.shark.sonar.controller.ProfileDbControl;
@@ -28,7 +33,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static Client client;
-    private int ConversationSize;
+    private int ConversationSize, notificationId = 0;
     private MainAdapter adapter;
     private Profile ProfUser;
     private ProfileDbControl ProfCon;
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtMainAddDesc;
     private SharedPreferences prefs;
     private boolean setPin = true;
-
+    private String CHANNEL_ID = "Sonar-Notification-Channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         con.destroy();
+        setUpNotificationChannel();
 
         prefs = this.getSharedPreferences("com.shark.sonar", Context.MODE_PRIVATE);
         setPin = !prefs.getString("pin", "").equals("");
@@ -85,7 +91,10 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
 
-            client = new Client(ProfUser.getUser_ID_key(), ProfUser.getUser_key_public(), ProfUser.getUser_key_private(), this);
+            int port = prefs.getInt("port", 0);
+            String IP = prefs.getString("IP", null);
+
+            client = new Client(ProfUser.getUser_ID_key(), ProfUser.getUser_key_public(), ProfUser.getUser_key_private(), IP, port);
 
             mainView = findViewById(R.id.imgPersonMain);
             mainView.setImageDrawable(ProfUser.getIcon().getIcon());
@@ -187,6 +196,50 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy(){
         super.onDestroy();
         ProfCon.destroy();
+    }
+
+    //REF https://developer.android.com/training/notify-user/build-notification
+    private void setUpNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = this.getResources().getString(R.string.channelName);
+            String description = this.getResources().getString(R.string.channelDesc);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = this.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+    }
+
+    public void notifyUser(String title, String content) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.sonar_alone)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
+    }
+
+    public void setIP(String IP, int Port) {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("IP", IP);
+        editor.putInt("port", Port);
+
+        editor.apply();
     }
 
 
--- app/src/main/java/com/shark/sonar/utility/Client.java
+++ app/src/main/java/com/shark/sonar/utility/Client.java
@@ -1,6 +1,5 @@
 package com.shark.sonar.utility;
 
-import android.DataContainer;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -37,23 +36,27 @@ public class Client implements ResultHandler {
     private DataHolder dataHolder;
     private MainActivity mainActivity;
     private MessageActivity currentMessageActivity;
-    private Context c;
     private boolean isActive;
-    private int notificationId = 0;
-    private String CHANNEL_ID = "Sonar-Notification_Channel";
-    boolean auth = false;
+    private boolean auth = false;
+    private String tempIP;
 
-    public Client(byte[] ID, byte[] publicKey, byte[] privateKey, Context c) {
-        this.c = c;
-        setUpNotificationChannel();
+    public Client(byte[] ID, byte[] publicKey, byte[] privateKey, String IP, int port) {
 
         UserHolder user = new UserHolder(ID, publicKey, privateKey);
         Base64Android b = new Base64Android();
 
         dataHolder = new DataHolder(publicKey, privateKey);
 
-        dataHolder.setPort(6000);
-        dataHolder.setIP("35.246.88.255");
+        dataHolder.setPort(port);
+        dataHolder.setIP(IP);
+        this.tempIP = IP;
+
+        dataHolder.setAuthServer();
+
+        if (!tempIP.equals(dataHolder.getIP())){
+            mainActivity.setIP(dataHolder.getIP(), dataHolder.getPort());
+        }
+
         dataHolder.setBase64(b);
         dataHolder.setServer(false);
 
@@ -117,7 +120,7 @@ public class Client implements ResultHandler {
             }
 
             if (!isActive) {
-                notifyUser("Message from: " + prof.getName(), msg);
+                mainActivity.notifyUser("Message from: " + prof.getName(), msg);
             }
 
             refreshMain();
@@ -175,41 +178,6 @@ public class Client implements ResultHandler {
 
     public void refreshMain() {
         mainActivity.updateList();
-    }
-
-    //REF https://developer.android.com/training/notify-user/build-notification
-    private void setUpNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = c.getResources().getString(R.string.channelName);
-            String description = c.getResources().getString(R.string.channelDesc);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = c.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-    }
-
-    private void notifyUser(String title, String content) {
-        Intent intent = new Intent(c, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(c, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(c, CHANNEL_ID)
-                .setSmallIcon(R.drawable.sonar_alone)
-                .setContentTitle(title)
-                .setContentText(content)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                .setOnlyAlertOnce(true)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(c);
-        notificationManager.notify(0, builder.build());
     }
 
     public void isActive(Boolean isActive) {
